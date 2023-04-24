aa0aea0087b6ef36e50e2f6c5f20ecb88ae331c5
michaelkho <michael.kho@paypay-corp.co.jp>
2023-04-24 17:44:24 +0900

    remove

--- Pods/Target Support Files/Pods-PayPayDiary-PayPayDiaryUITests/Pods-PayPayDiary-PayPayDiaryUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PayPayDiary_PayPayDiaryUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PayPayDiary_PayPayDiaryUITests
-@end
--- Pods/Target Support Files/Settings/Settings-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Settings : NSObject
-@end
-@implementation PodsDummy_Settings
-@end
