ea8394ad7e6d2d0c238910ec9072a720d85e60e1
michaelkho <michael.kho@paypay-corp.co.jp>
2023-04-24 11:30:20 +0900

    update code

--- Modules/Authentication/Authentication/Coordinator/AuthCoordinator.swift
+++ Modules/Authentication/Authentication/Coordinator/AuthCoordinator.swift
@@ -8,12 +8,16 @@
 import DiaryFoundation
 import UIKit
 
+public enum AuthCoordinatorDestination {
+    case login
+    case otp
+    case settings(profileId: String)
+    case home(sessionId: String)
+    case back
+}
+
 public protocol AuthCoordinatorProtocol {
-    func navigateToLoginViewController()
-    func navigateToOTPViewController(viewModel: OTPViewModelProtocol)
-    func navigateToSettingsPage(profileId: String)
-    func navigateToDiaryHome(sessionId: String)
-    func popViewController()
+    func navigate(_ destination: AuthCoordinatorDestination)
 }
 
 public final class AuthCoordinator: BaseCoordinator, AuthCoordinatorProtocol {
@@ -27,38 +31,52 @@ public final class AuthCoordinator: BaseCoordinator, AuthCoordinatorProtocol {
     }
 
     public override func start() {
+        // if this coordinator have multi entrypoint
+        // add validation here and use navigate function
         guard secretPassword == "test" else { return }
-        navigateToLoginViewController()
+        navigate(.login)
+    }
+
+    public func navigate(_ destination: AuthCoordinatorDestination) {
+        switch destination {
+        case .login:
+            navigateToLoginViewController()
+        case .otp:
+            navigateToOTPViewController()
+        case .settings(let profileId):
+            navigateToSettingsPage(profileId: profileId)
+        case .home(let sessionId):
+            navigateToDiaryHome(sessionId: sessionId)
+        case .back:
+            navigationController.popViewController(animated: true)
+        }
     }
 }
 
-extension AuthCoordinator {
-    public func navigateToLoginViewController() {
+private extension AuthCoordinator {
+    func navigateToLoginViewController() {
         let vm = AuthViewModel(coordinator: self)
         let vc = AuthViewController(viewModel: vm)
         navigationController.pushViewController(vc, animated: true)
     }
 
-    public func navigateToOTPViewController(viewModel: OTPViewModelProtocol) {
+    func navigateToOTPViewController() {
+        let viewModel = OTPViewModel(coordinator: self)
         let vc = OTPViewController(viewModel: viewModel)
         navigationController.pushViewController(vc, animated: true)
     }
 
-    public func navigateToSettingsPage(profileId: String) {
+    func navigateToSettingsPage(profileId: String) {
         guard let appCoordinator = getAppCoordinator() else { return }
-        appCoordinator.navigateTo(destination: .settingPage(profileId: "profileId"))
+        appCoordinator.navigateTo(destination: .settingPage(profileId: profileId))
     }
 
-    public func navigateToDiaryHome(sessionId: String) {
+    func navigateToDiaryHome(sessionId: String) {
         guard let appCoordinator = getAppCoordinator() else { return }
         appCoordinator.navigateTo(destination: .homePage(
-            name: "some-name",
+            name: "some-name-and-\(sessionId)",
             accessKey: "some-accessKey"))
 
         parent?.removeChildCoordinator(self)
-    }
-
-    public func popViewController() {
-        navigationController.popViewController(animated: true)
     }
 }
